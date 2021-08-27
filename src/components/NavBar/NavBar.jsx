5bcb091076325449474f02881d3141de7037dd19
kieranjlynch <k.j.lynch@icloud.com>
2021-08-27 12:11:02 +0100

    further changes for mobile responsiveness

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,20 +6,6 @@ const NavBar = ({ searchTerm, updateSeachTerm, placeHolder }) => {
   return (
     <div className={styles.nav}>
       <input type="text" value={searchTerm} onChange={updateSeachTerm} placeholder={placeHolder} />
-        <div className={styles.filterContainer}>
-              {/* <div className={styles.checkbox1}>
-              <label className={styles.label} for="ABV">High ABV (6.0% +)</label>
-              <input className={styles.checkbox} type="checkbox" id="ABV"/>
-              </div>
-              <div className={styles.checkbox2}>
-              <label className={styles.label} for="Acidic">Acidic (ph under 4)</label>
-              <input className={styles.checkbox} type="checkbox" id="Classic"/>
-              </div>
-              <div className={styles.checkbox3}>
-              <label className={styles.label} for="Classic">Classic Range</label>
-              <input className={styles.checkbox} type="checkbox" id="Acidic"/>
-              </div> */}
-        </div>
     </div>
   );
 };
